1bcafb31f76e4719db6bd423fa861d23588db668
Gusttavo Ruy <7leckler@gmail.com>
2024-08-25 23:57:11 -0300

    fix: timer

--- App.java
+++ App.java
@@ -1,23 +1,35 @@
 import java.awt.Robot;
 import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
 
 public class App {
   public static void main(String[] args) throws Exception {
     Robot robot = new Robot();
-    Boolean rest = false;
+    Boolean rest = true;
+    boolean side = false;
     int count = 0;
     while (true) {
       while(rest) {
         count++;
+        if(side) {
+          robot.keyPress(KeyEvent.VK_D);
+        } else {
+          robot.keyPress(KeyEvent.VK_A);
+        }
         robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
         robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
         Thread.sleep(300);
         if(count >= 15) {
           rest = false;
+          side = !side;
           count = 0;
+          robot.keyRelease(KeyEvent.VK_A);
+          robot.keyRelease(KeyEvent.VK_D);
         }
       }
-      Thread.sleep(5000);
+      if(side) {
+        Thread.sleep(25000);
+      }
       rest = true;
     }
   }
